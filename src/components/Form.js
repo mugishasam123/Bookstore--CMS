d664731ec99296eda0c6eb4f2d26873e6504248e
mugisha Samuel <mugishasamuel42@gmail.com>
2022-02-22 09:54:34 -0800

    Add create and remove book functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Form = () => (
-  <section className="form">
-    <h3> ADD NEW BOOK </h3>
-    {' '}
-    <form>
-      <input type="text" placeholder="Book title" required />
-      <select>
-        <option value="action"> Action </option>
-        {' '}
-        <option value="comedy"> Comedy </option>
-        {' '}
-        <option value="horror"> Horror </option>
-        {' '}
-        <option value="fiction"> Fiction </option>
-        {' '}
-        <option value="classics"> Classics </option>
-        {' '}
-      </select>
-      {' '}
-      <button type="submit"> ADD BOOK </button>
-      {' '}
-    </form>
-    {' '}
-  </section>
-);
+import { useDispatch, useSelector } from 'react-redux';
+import { addBook } from '../redux/books/books';
+
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.books);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: `item_${data.length + 1}`,
+      title,
+      category,
+    };
+    dispatch(addBook(newBook));
+    setTitle('');
+    setCategory('');
+  };
+
+  return (
+    <section className="form">
+      <h3> ADD NEW BOOK </h3>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Book title"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="action"> Action </option>
+
+          <option value="comedy"> Comedy </option>
+
+          <option value="horror"> Horror </option>
+
+          <option value="fiction"> Fiction </option>
+
+          <option value="classics"> Classics </option>
+        </select>
+
+        <button type="submit"> ADD BOOK </button>
+      </form>
+    </section>
+  );
+};
 
 export default Form;
