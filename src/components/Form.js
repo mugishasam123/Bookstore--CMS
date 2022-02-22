d205e9cf27ba98d5309310aa96db5939c394315f
mugisha Samuel <mugishasamuel42@gmail.com>
2022-02-22 01:49:22 -0800

    Add re usable components

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,18 +2,27 @@ import React from 'react';
 
 const Form = () => (
   <section className="form">
-    <h3>ADD NEW BOOK</h3>
+    <h3> ADD NEW BOOK </h3>
+    {' '}
     <form>
       <input type="text" placeholder="Book title" required />
-      <select value="category">
-        <option value="action">Action</option>
-        <option value="comedy">Comedy</option>
-        <option value="horror">Horror</option>
-        <option value="fiction">Fiction</option>
-        <option value="classics">Classics</option>
+      <select>
+        <option value="action"> Action </option>
+        {' '}
+        <option value="comedy"> Comedy </option>
+        {' '}
+        <option value="horror"> Horror </option>
+        {' '}
+        <option value="fiction"> Fiction </option>
+        {' '}
+        <option value="classics"> Classics </option>
+        {' '}
       </select>
-      <button type="submit">ADD BOOK</button>
+      {' '}
+      <button type="submit"> ADD BOOK </button>
+      {' '}
     </form>
+    {' '}
   </section>
 );
 
